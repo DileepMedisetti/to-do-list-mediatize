7410c64c40abe8b281e043b8d85098b21f7a2970
Dileepmedisetti <dileepmedisetti1@gmail.com>
2026-09-01 21:20:22 +0530

    added dotenv

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: "https://to-do-list-mediatize.onrender.com",
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         "Content-Type": "application/json"
     }
