225e4617b0e522ceb52ab7f2d3b3e26eb55e4276
Dileepmedisetti <dileepmedisetti1@gmail.com>
2026-09-02 12:42:16 +0530

    changed server to vercel

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ app = FastAPI(title="To Do List Crud Project",
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://to-do-list-mediatize.netlify.app",
+    "https://to-do-list-mediatize.vercel.app",
 ]
 
 
